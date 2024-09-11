6f1473b048a57e3c27e57b79340682ab15a48a35
root <schimea@hotmail.com>
2024-09-11 11:01:25 -0700

    Transform lowercase to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for char in str:
+    for i, char in enumerate(str):
         if ord(char) in range(97, 123):
             upper_char = chr(ord(char) - 32)
-            print("{}".format(upper_char), end="" if char != len(str) - 1 else "\n")
+            print("{}".format(upper_char), end="" if i != len(str) - 1 else "\n")
         else:
-            print("{}".format(char), end="" if char != len(str) - 1 else "\n")
+            print("{}".format(char), end="" if i != len(str) - 1 else "\n")
