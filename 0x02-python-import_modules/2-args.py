a030a931a5da96f4edc139b54c021ffd95dc81d3
root <schimea@hotmail.com>
2024-09-13 01:57:07 -0700

    Print all arguments passed in

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    if len(argv) > 0:
+    if len(argv) == 1:
+        print("{} argument:".format(len(argv)), end="\n")
+        for index, value in enumerate(argv, start=1):
+            print("{}: {}".format(index, value))
+    elif len(argv) > 1:
         print("{} arguments:".format(len(argv)), end="\n")
         for index, value in enumerate(argv, start=1):
             print("{}: {}".format(index, value))
