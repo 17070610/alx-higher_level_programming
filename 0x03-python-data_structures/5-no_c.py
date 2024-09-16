a5ac46bcecf0ded4d624f243501ffcf6ee56a9a6
root <schimea@hotmail.com>
2024-09-16 13:08:39 -0700

    Removes c and C from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 if __name__ == "__main__":
     def no_c(my_string):
         new_str = ""
         for char in my_string:
             if char != 'c' and char != 'C':
                 new_str += char
-         return new_str
+        return new_str
