5acbceccfafb02d58cf20f188d0014ed9675c272
17070610 <schimea@hotmail.com>
2024-09-20 04:55:30 -0700

    Deletes a key in a dictionary

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    for init_key in a_dictionary:
+        if init_key == key:
+            del a_dictionary[init_key]
+    return a_dictionary
