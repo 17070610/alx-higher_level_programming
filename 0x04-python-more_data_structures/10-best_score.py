aac9b473a95b78d4501ebdbee79f17c130b01758
17070610 <schimea@hotmail.com>
2024-09-20 05:24:08 -0700

    Returns a key with the best integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     best_score = 0
     for key, value in a_dictionary.items():
         if (value > best_score):
-            best_score = value
+            best_score = key
         else:
             return None
     return best_score
