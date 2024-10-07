5203769d578ab2853127e87f8d153aaac65dcfa3
17070610 <schimea@hotmail.com>
2024-10-07 11:29:42 -0700

    Change the represantation to any symbo;

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -86,7 +86,8 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        return '\n'.join(str(self.print_symbol) * self.width for _ in range(self.height))
+        return '\n'.join(str(self.print_symbol) *
+                         self.width for _ in range(self.height))
 
     def __repr__(self):
         """
