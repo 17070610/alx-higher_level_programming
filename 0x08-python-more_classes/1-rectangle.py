11111014e8ea2c049c53148ddcfcd69a2a48470a
17070610 <schimea@hotmail.com>
2024-10-07 10:29:36 -0700

    Sets the value of the height and width of the rectangle and returns its area and perimeter

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+
+"""
+This module contains a class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    This class defines a rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Instantiation with optional width and height
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        Retrieves the width of the rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Sets the value of the width of the rectangle
+
+        Raises:
+            TypeError: When value is not an integer
+            ValueError: When value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Retrieves the height of the recatangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Sets the value of the height of the rectangle
+
+        Raises:
+            TypeError: When the value is not an integer
+            ValueError: When the value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        Returns the area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Returns the perimeter of the rectangle
+        """
+        perimeter = 2 * (self.__width + self.__height)
+        if (self.__width == 0 or self.__height == 0):
+            perimeter = 0
+
+        return perimeter
