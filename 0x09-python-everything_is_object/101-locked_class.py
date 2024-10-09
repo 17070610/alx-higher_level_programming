413dfee07f90adf214084f83806f62805d7b7b71
17070610 <schimea@hotmail.com>
2024-10-09 01:28:11 -0700

    Locked Class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines a class LockedClass
+"""
+
+
 class LockedClass:
+    """
+    Locked class that allows the user to only create no new instance except
+    first class
+    """
     __slots__ = ['first_name']
