31a8b4d78fb0acdd9ee0bad4a5dcc5d0c29dd722
17070610 <schimea@hotmail.com>
2024-10-11 04:26:51 -0700

    Returns the sum of two integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""
+Defines an integer addition function
+"""
+
 
 def add_integer(a, b=98):
+    """
+    Returns the integer sum of a and b
+
+    Float arguments are turned into ints before adding them
+
+    Raises:
+        TypeError: If a or b is neither int or float
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
