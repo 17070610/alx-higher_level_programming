80090349e4a3a803468fb513785deceb9c03f1f9
17070610 <schimea@hotmail.com>
2024-10-14 10:04:51 -0700

    Checks if obj inherits directly or indirectly from a_class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,6 +9,6 @@ def inherits_from(obj, a_class):
     Checks if obj inherits directly or indirectly from a_class
     """
 
-    if issubclass(obj, a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
