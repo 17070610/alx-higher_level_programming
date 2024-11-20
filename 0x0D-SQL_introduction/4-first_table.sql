af3e869965e5d5f9699267ae7f631687c9031297
17070610 <schimea@hotmail.com>
2024-11-20 01:02:07 -0800

    Creatse a table first_table of my current database in my MySQL server

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Creatse a table called first_table in the current database in my MySQL server
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL	
+);
