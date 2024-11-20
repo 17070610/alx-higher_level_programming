0802c850ee1700deb8642df1695868fe86967c8d
17070610 <schimea@hotmail.com>
2024-11-20 09:28:30 -0800

    Create a full table second_table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,14 +1,7 @@
 -- creating a full table with rows and data in it
 
-CREATE TABLE IF NOT EXISTS second_table (
-	id INT PRIMARY KEY,
-	name VARCHAR(255) NOT NULL,
-	score INT NOT NULL
-);
-
-INSERT INTO second_table (id, name, score)
-VALUES
-	(1, 'John', 10),
-	(2, 'Alex', 3),
-	(3, 'Bob', 14),
-	(4, 'Goerge', 8);
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
