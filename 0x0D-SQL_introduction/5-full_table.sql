e4c71d60829122c3579549c11ce3bc37f4ec244b
17070610 <schimea@hotmail.com>
2024-11-20 01:14:33 -0800

    Prints the full description of the table first_table from database hbtn_0c_0 in my MySQL server

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,11 @@
+-- full database description of the table first_table from the database
+-- hbtn_0c_0 in my MySQL server
+
+SELECT 	column_name,
+	column_type,
+	is_nullable,
+	column_default,
+	extra
+FROM information_schema.columns
+WHERE table_schema = 'hbtn_0c_0'
+AND table_name = 'first_table';
