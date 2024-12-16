cf7a8119041cbbc02a1b8a1fc8c87a7a72d5ef1e
root <schimea@hotmail.com>
2024-12-16 10:41:44 -0800

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 let i;
 
 if (isNaN(parseInt(args[0], 10))) {
-  console.log('Missing number of occurences');
+  console.log('Missing size');
 } else {
   const size = parseInt(args[0], 10);
   for (i = 0; i < size; i++) {
