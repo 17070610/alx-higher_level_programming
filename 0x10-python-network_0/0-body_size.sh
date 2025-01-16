e1dfc0e2da71e4b62e0ad5eee45eed5f9608e7de
17070610 <schimea@hotmail.com>
2025-01-16 03:29:25 -0800

    Curl body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to it and displays
-# the size of the bodyof the response
+# Takes in a URL, sends a request to it and displays the size of the bodyof the response
 curl -s "$1" -o /dev/null -w '%{size_download}\n'
