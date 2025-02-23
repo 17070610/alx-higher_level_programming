31236047469d90add9ad5364a7a29ea69b4105bb
17070610 <schimea@hotmail.com>
2025-02-22 21:16:14 -0800

    Say hello to everybody!

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $("#btn_translate").click(function () {
     const langCode = $("#language_code").val().trim();
     if (langCode) {
-      const url = `https://ww.fourtonfish.com/hellosalut/hello/?lang=$(langCode)`;
+      const url = `https://www.fourtonfish.com/hellosalut/hello/?lang=${langCode}`;
 
       $.get(url, function (data) {
         $("#hello").text(data.hello);
