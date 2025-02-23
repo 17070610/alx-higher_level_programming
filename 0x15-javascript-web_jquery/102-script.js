b7335e115cdb384a0f8ef29c82fb9e23e0078e5e
17070610 <schimea@hotmail.com>
2025-02-22 20:59:37 -0800

    Say hello to everybody!

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $("#btn_translate").click(function () {
+    const langCode = $("#language_code").val().trim();
+    if (langCode) {
+      const url = `https://ww.fourtonfish.com/hellosalut/hello/?lang=$(langCode)`;
+
+      $.get(url, function (data) {
+        $("#hello").text(data.hello);
+      }).fail(function() {
+        $("#hello").text("Error: Unable to fetch translation");
+      });
+    }
+  });
+});
